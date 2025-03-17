8d08f2172e4344cb2e1f1ffb8b9aca256a3ed512
Mate Kukri <mate.kukri@canonical.com>
2025-03-17 13:20:09 +0000

    fs/xfs: Propagate incorrect inode error from grub_xfs_read_inode
    Gbp-Pq: cve-2025-jan/fs-xfs-Propagate-incorrect-inode-error-from-grub_xfs_read.patch.

--- grub-core/fs/xfs.c
+++ grub-core/fs/xfs.c
@@ -806,7 +806,6 @@ static int iterate_dir_call_hook (grub_uint64_t ino, const char *filename,
   fdiro = grub_malloc (sz);
   if (!fdiro)
     {
-      grub_print_error ();
       return 0;
     }
 
@@ -818,7 +817,6 @@ static int iterate_dir_call_hook (grub_uint64_t ino, const char *filename,
   err = grub_xfs_read_inode (ctx->diro->data, ino, &fdiro->inode);
   if (err)
     {
-      grub_print_error ();
       grub_free (fdiro);
       return 0;
     }
@@ -858,9 +856,13 @@ grub_xfs_iterate_dir (grub_fshelp_node_t dir,
 	/* Synthesize the direntries for `.' and `..'.  */
 	if (iterate_dir_call_hook (diro->ino, ".", &ctx))
 	  return 1;
+	else if (grub_errno)
+	  return 0;
 
 	if (iterate_dir_call_hook (parent, "..", &ctx))
 	  return 1;
+	else if (grub_errno)
+	  return 0;
 
 	for (i = 0; i < head->count &&
 	     (grub_uint8_t *) de < ((grub_uint8_t *) dir + grub_xfs_fshelp_size (dir->data)); i++)
@@ -900,6 +902,9 @@ grub_xfs_iterate_dir (grub_fshelp_node_t dir,
 		return 1;
 	      }
 	    de->name[de->len] = c;
+
+	    if (grub_errno)
+	      return 0;
 
 	    de = grub_xfs_inline_next_de(dir->data, head, de);
 	  }
@@ -997,6 +1002,11 @@ grub_xfs_iterate_dir (grub_fshelp_node_t dir,
 		  {
 		    grub_free (dirblock);
 		    return 1;
+		  }
+		else if (grub_errno)
+		  {
+		    grub_free (dirblock);
+		    return 0;
 		  }
 
 		/*
